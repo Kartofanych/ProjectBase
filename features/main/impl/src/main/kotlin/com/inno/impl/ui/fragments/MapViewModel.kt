390dcb885b9f2ec59269b351656b404aba78be58
79196 <kartofanych@gmail.com>
2024-06-09 21:59:03 +0300

    hot fix

--- features/main/impl/src/main/kotlin/com/inno/impl/ui/fragments/MapViewModel.kt
+++ features/main/impl/src/main/kotlin/com/inno/impl/ui/fragments/MapViewModel.kt
@@ -2,11 +2,8 @@ package com.inno.impl.ui.fragments
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.Color
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import com.example.common.di.AppContext
 import com.example.multimodulepractice.main.impl.R
@@ -17,8 +14,7 @@ import com.yandex.mapkit.geometry.LinearRing
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.geometry.Polygon
 import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.MapObjectCollection
-import com.yandex.mapkit.map.PlacemarkMapObject
+import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.map.PolygonMapObject
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.runtime.image.ImageProvider
@@ -32,7 +28,9 @@ class MapViewModel @Inject constructor(
     private val mapView: MapView
 ) : ViewModel() {
 
-    init {
+    private val placemarkTapListener = MapObjectTapListener { _, point ->
+        Toast.makeText(context, "Иннополис", Toast.LENGTH_SHORT).show()
+        true
     }
 
     fun onStart() {
@@ -52,35 +50,16 @@ class MapViewModel @Inject constructor(
 
 
     private fun setMarkerInStartLocation() {
-        val marker = createBitmapFromVector(R.drawable.ic_pin_black_svg)
-        val mapObjectCollection: MapObjectCollection = mapView.mapWindow.map.mapObjects
-        //TODO текущую локацию
-        val placemarkMapObject: PlacemarkMapObject = mapObjectCollection.addPlacemark(
-            Point(55.752085, 48.744618),
-            ImageProvider.fromBitmap(marker)
-        )
-        placemarkMapObject.opacity = 0.5f
-        placemarkMapObject.addTapListener { _, _ ->
-            Toast.makeText(context, "Иннополис", Toast.LENGTH_SHORT).show()
-            true
+        val mapObjectCollection = mapView.mapWindow.map.mapObjects
+        val obj = mapObjectCollection.addPlacemark().apply {
+            geometry = Point(55.752085, 48.744618)
+            setIcon(ImageProvider.fromResource(context, R.drawable.ic_dollar_pin))
         }
-    }
-
-    private fun createBitmapFromVector(art: Int): Bitmap? {
-        val drawable = ContextCompat.getDrawable(context, art) ?: return null
-        val bitmap = Bitmap.createBitmap(
-            drawable.intrinsicWidth,
-            drawable.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
-        )
-        val canvas = Canvas(bitmap)
-        drawable.setBounds(0, 0, canvas.width, canvas.height)
-        drawable.draw(canvas)
-        return bitmap
+        obj.addTapListener(placemarkTapListener)
     }
 
     private fun drawBoundary() {
-        val mapObjects: MapObjectCollection = mapView.mapWindow.map.mapObjects.addCollection()
+        val mapObjects = mapView.mapWindow.map.mapObjects.addCollection()
         val outerBoundaryCoordinates = listOf(
             Point(89.0, -170.0),
             Point(89.0, 170.0),
@@ -107,6 +86,5 @@ class MapViewModel @Inject constructor(
         mapView.onStop()
         MapKitFactory.getInstance().onStop()
     }
-
 
 }
