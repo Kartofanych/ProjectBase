e42a12dc411ab0f917fcbbb14aa1fd55ce4c487f
79196 <kartofanych@gmail.com>
2024-07-14 14:38:42 +0300

    release build

---
+++ features/main/impl/src/main/kotlin/com/example/multimodulepractice/main/impl/ui/compose_elements/BigHorizontalAttraction.kt
@@ -0,0 +1,132 @@
+package com.example.multimodulepractice.main.impl.ui.compose_elements
+
+import android.content.Context
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.request.ImageRequest
+import coil.size.Scale
+import com.example.multimodulepractice.common.composables.shimmerBrush
+import com.example.multimodulepractice.common.theme.mediumTextStyle
+import com.example.multimodulepractice.common.theme.regularTextStyle
+import com.example.multimodulepractice.common.theme.semiboldTextStyle
+import com.example.multimodulepractice.landmark.ui.landmark_content.Chip
+import com.example.multimodulepractice.main.impl.data.local_models.list.CloseAttraction
+import com.example.multimodulepractice.main.impl.ui.list.ListAction
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun BigHorizontalAttraction(
+    context: Context,
+    attraction: CloseAttraction,
+    onListAction: (ListAction) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(11.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .clickable {
+                onListAction(ListAction.OpenAttraction(attraction.id))
+            }
+            .padding(bottom = 12.dp),
+    ) {
+
+        LoadingImage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(2f)
+                .clip(RoundedCornerShape(16.dp)),
+            attraction.icon,
+            context
+        )
+
+        Text(
+            text = attraction.name,
+            style = semiboldTextStyle.copy(fontSize = 16.sp),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        FlowRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            for (category in attraction.categories) {
+                Chip(text = category.name, color = category.color)
+            }
+        }
+
+        Text(
+            text = attraction.shortInfo,
+            style = mediumTextStyle.copy(fontSize = 14.sp),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        Text(
+            text = "Дата основания: ${attraction.dateCreation}",
+            style = regularTextStyle.copy(fontSize = 12.sp, color = Color(0xFF959595)),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun LoadingImage(modifier: Modifier, url: String, context: Context) {
+    SubcomposeAsyncImage(
+        model = ImageRequest.Builder(context)
+            .data(url)
+            .scale(Scale.FILL)
+            .build(),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = modifier,
+    ) {
+        val state = painter.state
+        AnimatedVisibility(visible = state is AsyncImagePainter.State.Success) {
+            SubcomposeAsyncImageContent()
+        }
+        AnimatedVisibility(visible = state !is AsyncImagePainter.State.Success) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        shimmerBrush(
+                            targetValue = 1300f,
+                            showShimmer = true
+                        )
+                    )
+            )
+        }
+    }
+}
--- features/main/impl/src/main/kotlin/com/example/multimodulepractice/main/impl/ui/list/ListScreen.kt
+++ features/main/impl/src/main/kotlin/com/example/multimodulepractice/main/impl/ui/list/ListScreen.kt
@@ -1,16 +1,9 @@
 package com.example.multimodulepractice.main.impl.ui.list
 
-import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -29,19 +22,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.example.multimodulepractice.common.theme.mediumTextStyle
 import com.example.multimodulepractice.common.theme.semiboldTextStyle
 import com.example.multimodulepractice.main.impl.R
-import com.example.multimodulepractice.main.impl.data.local_models.list.CloseAttraction
-import com.example.multimodulepractice.main.impl.data.local_models.list.VerticalAttraction
+import com.example.multimodulepractice.main.impl.ui.compose_elements.BigHorizontalAttraction
+import com.example.multimodulepractice.main.impl.ui.compose_elements.VerticalAttractionView
 
 @Composable
 fun ListScreen(uiState: ListUiState, onListAction: (ListAction) -> Unit) {
@@ -56,7 +45,6 @@ fun ListScreen(uiState: ListUiState, onListAction: (ListAction) -> Unit) {
                 .background(Color(0xFFF6F6F6))
         ) {
             item {
-
                 Text(
                     text = "Находите \nновое с нами",
                     style = semiboldTextStyle.copy(fontSize = 24.sp),
@@ -135,177 +123,15 @@ fun ListScreen(uiState: ListUiState, onListAction: (ListAction) -> Unit) {
                     key = {
                         it.id
                     }
-                ) {
-                    HorizontalAttraction(
+                ) { item ->
+                    BigHorizontalAttraction(
                         context = context,
-                        attraction = it,
+                        attraction = item,
                         onListAction = onListAction
                     )
                     Spacer(modifier = Modifier.height(22.dp))
                 }
             }
         }
-    }
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun HorizontalAttraction(
-    context: Context,
-    attraction: CloseAttraction,
-    onListAction: (ListAction) -> Unit
-) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(160.dp)
-            .background(color = Color.White, shape = RoundedCornerShape(16.dp))
-            .clip(RoundedCornerShape(16.dp))
-            .clickable {
-                onListAction(ListAction.OpenAttraction(attraction.id))
-            }
-            .padding(top = 13.dp)
-            .padding(start = 14.dp, end = 8.dp),
-    ) {
-        Row {
-            Column(
-                modifier = Modifier
-                    .width(208.dp)
-            ) {
-
-                Text(
-                    text = attraction.name,
-                    style = semiboldTextStyle.copy(fontSize = 14.sp),
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-
-                Text(
-                    text = attraction.name,
-                    style = mediumTextStyle.copy(fontSize = 11.sp),
-                    maxLines = 3,
-                    overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier.padding(top = 10.dp)
-                )
-
-                FlowRow(
-                    modifier = Modifier
-                        .padding(top = 8.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    maxLines = 1
-                ) {
-                    for (category in attraction.categories) {
-                        CategoryChip(text = category.name, color = category.color)
-                    }
-                }
-
-                Text(
-                    text = attraction.dateCreation,
-                    style = mediumTextStyle.copy(fontSize = 11.sp, color = Color(0xFF959595)),
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier.padding(top = 10.dp)
-                )
-            }
-            AsyncImage(
-                model = ImageRequest.Builder(context)
-                    .data(attraction.icon)
-                    .build(),
-                contentScale = ContentScale.Crop,
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(bottom = 13.dp)
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .clip(RoundedCornerShape(12.dp))
-            )
-        }
-
-    }
-
-}
-
-@Composable
-fun CategoryChip(text: String, color: Color) {
-    Box(
-        modifier = Modifier
-            .background(color = color, shape = RoundedCornerShape(5.dp))
-            .padding(horizontal = 4.dp, vertical = 2.dp)
-            .height(14.dp),
-    ) {
-        Text(
-            text = text,
-            color = Color.White,
-            style = semiboldTextStyle.copy(fontSize = 10.sp),
-            modifier = Modifier
-                .fillMaxHeight()
-                .align(Alignment.Center)
-        )
-    }
-}
-
-
-@Composable
-fun VerticalAttractionView(
-    attraction: VerticalAttraction,
-    context: Context,
-    onListAction: (ListAction) -> Unit
-) {
-    Box(
-        modifier = Modifier
-            .height(270.dp)
-            .width(170.dp)
-            .background(color = Color.White, shape = RoundedCornerShape(20.dp))
-            .clip(RoundedCornerShape(20.dp))
-            .clickable {
-                onListAction(ListAction.OpenAttraction(attraction.id))
-            }
-    ) {
-        AsyncImage(
-            model = ImageRequest.Builder(context)
-                .data(attraction.icon)
-                .build(),
-            contentScale = ContentScale.Crop,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(170.dp)
-        )
-        Text(
-            text = attraction.name,
-            style = semiboldTextStyle.copy(fontSize = 14.sp),
-            modifier = Modifier
-                .padding(top = 182.dp)
-                .padding(horizontal = 7.dp)
-                .fillMaxWidth(),
-            maxLines = 3,
-            overflow = TextOverflow.Ellipsis
-        )
-
-        Row(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 16.dp)
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_location),
-                contentDescription = null,
-                modifier = Modifier.size(18.dp),
-                tint = Color(0xFF4779D8)
-            )
-
-            Text(
-                text = attraction.distance,
-                style = mediumTextStyle.copy(
-                    color = Color(0xFF535353),
-                    fontSize = 13.sp
-                ),
-            )
-        }
-
     }
 }
